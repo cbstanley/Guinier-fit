9494172e53711572067741772e1e881f8b6d62a4
Chris Stanley <cbstanley@users.noreply.github.com>
2018-04-23 21:57:45 -0400

    Update guinier_fit.py
    Include error calculation in fit

--- guinier_fit.py
+++ guinier_fit.py
@@ -2,77 +2,89 @@
 # -*- coding: utf-8 -*-
 
 """
-    Simple script to perform a Guinier fit on small-angle scattering data
+Simple script to perform a Guinier fit on small-angle scattering data
 
-    Fit values:
-    I(0) = intensity extrapolated to zero-angle
-    Rg = radius of gyration
+Fit values:
+I(0) = intensity extrapolated to zero-angle
+Rg = radius of gyration
 
-    Relevant fit range is when q_max * Rg < 1.3
+Relevant fit range is when q_max * Rg < 1.3
 """
 
 import numpy as np
+from scipy import optimize
 import matplotlib.pyplot as plt
 
 
 def read_datafile(f):
+    """
+    Read in data as comma delimited
+    """
     data = np.loadtxt(f, delimiter=',', skiprows=2)
     return data
+
+
+def line(x, a, b):
+    """
+    Fit function for optimize.curvefit
+    """
+    return a*x + b
 
 
 # Example scattering data on a protein
 data = read_datafile('protein_sans.txt')
 
-x = data[:, 0]
-y = data[:, 1]
+xdata = data[:, 0]
+ydata = data[:, 1]
+yerr = data[:, 2]
 
-# Set q_min and q_max for fit range
-q_min = 0.016
+# Set q_min and q_max for fit range of interest
+q_min = 0.02
 q_max = 0.043
 
-# Parameter to adjust plot range
-plot_more = 0.6
-
-cond_data = (x >= q_min * (1 - plot_more)) & (x <= q_max * (1 + plot_more))
-x = x[cond_data]
-y = y[cond_data]
-
-cond_fit = (x >= q_min) & (x <= q_max)
-xFit = x[cond_fit]
-yFit = y[cond_fit]
+# Optional: Adjust the plot zoom-out (0 = no zoom out)
+xpand = 0.2
 
 # Linearize data to ln[I(q)] vs. q^2
-x = x**2
-y = np.log(y)
+x2 = xdata**2
+logy = np.log(ydata)
+logyerr = yerr / ydata
 
-xFit = xFit**2
-yFit = np.log(yFit)
+cond_fit = (xdata >= q_min) & (xdata <= q_max)
+x2_xlim = x2[cond_fit]
+logy_xlim = logy[cond_fit]
+logyerr_xlim = logyerr[cond_fit]
 
-p, cov = np.polyfit(xFit, yFit, 1, cov=True)
-yFit = np.polyval(p, xFit)
+popt, pcov = optimize.curve_fit(line, x2_xlim, logy_xlim, sigma=logyerr_xlim)
+
+Err_a = pcov[0, 0]**0.5
+Err_b = pcov[1, 1]**0.5
 
 # Calculate I(0) and Rg values from fit
-I0 = np.exp(p[1])
-Rg = np.sqrt(-3 * p[0])
+I0 = np.exp(popt[1])
+Rg = np.sqrt(-3 * popt[0])
 qmaxRg = q_max * Rg
-
-# To Do: Add error calculations
-# I0_err =
-# Rg_err =
+I0_err = abs(np.exp(popt[1]) * Err_b)
+Rg_err = abs(-3/2 * (-3 * popt[0])**(-0.5) * Err_a)
 
 # Create Guinier plot with fit
 plt.clf()
-plt.plot(x, y, 'x')
-plt.plot(xFit, yFit)
-plt.xlabel(r'${q^2}$ ($\AA^{-2}$)', fontsize=14)
+plt.plot(x2_xlim, line(x2_xlim, popt[0], popt[1]))  # Fit
+plt.errorbar(x2, logy, yerr=logyerr, fmt='k.')  # Data
+plt.title('Guinier Fit')
+plt.xlabel('$q^2$ ($\AA^{-2}$)', fontsize=14)
 plt.ylabel(r'$ln[I(q)]$', fontsize=14)
-plt.title('Guinier Plot', fontsize=14)
+plt.xlim(x2_xlim[0] * (1 - xpand), x2_xlim[-1] * (1 + xpand))
+plt.ylim(logy_xlim[-1] * (1 + xpand), logy_xlim[0] * (1 - xpand))
 
-results = ('y = a x + b' +
-           '\n' + 'a = ' + str(round(p[0], 2)) + ', b = ' + str(round(p[1], 4)) +
-           '\n'
-           '\n' + 'I(0) = ' + str(round(I0, 4)) +
-           '\n' + '$R_g$ = ' + str(round(Rg, 2)) + ' $\AA$' +
+results = ('y = a x + b' + '\n' +
+           'a = ' + str(round(popt[0], 2)) +
+           ', b = ' + str(round(popt[1], 4)) + '\n'
+           '\n' +
+           'I(0) = ' + str(round(I0, 4)) + ' $\pm$ ' + str(round(I0_err, 4)) +
+           '\n' +
+           '$R_g$ = ' + str(round(Rg, 2)) + ' $\pm$ ' + str(round(Rg_err, 2)) +
+           ' $\AA$' +
            '\n' + '$q_{max}$$R_g$ = ' + str(round(qmaxRg, 2))
            )
 
@@ -84,8 +96,8 @@ plt.show(block=True)
 
 # Also print out results
 print('Fit using y= a x + b')
-print('a =', round(p[0], 2), 'b =', round(p[1], 4))
+print('a =', round(popt[0], 2), 'b =', round(popt[1], 4))
 print('q_min^2 =', round(q_min**2, 6), ',', 'q_max^2 =', round(q_max**2, 6))
-print('I(0) =', round(I0, 4))
-print('Rg =', round(Rg, 2))
+print('I(0) =', round(I0, 4), '+/-', round(I0_err, 4))
+print('Rg =', round(Rg, 2), '+/-', round(Rg_err, 2))
 print('q_max*Rg =', round(qmaxRg, 2))
